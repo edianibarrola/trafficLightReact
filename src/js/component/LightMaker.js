19a371907f02149fb35cb6094ce8d0ccafc410ce
edianibarrola <edianibarrola@gmail.com>
2020-12-11 23:19:43 +0000

    update

--- src/js/component/LightMaker.js
+++ src/js/component/LightMaker.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 export const LightMaker = () => {
+	const [clickedLight, setClickedLight] = useState(null);
+	let turnOnRed = "";
+	if (clickedLight == "red") turnOnRed = "lightOnRed";
+	let turnOnYellow = "";
+	if (clickedLight == "yellow") turnOnYellow = "lightOnYellow";
+	let turnOnGreen = "";
+	if (clickedLight == "green") turnOnGreen = "lightOnGreen";
 	return (
 		<div>
 			<div className="lightBody">
-				<div className="rounded-circle redLight lightOnRed" />
-				<div className="rounded-circle yellowLight " />
-				<div className="rounded-circle greenLight " />
+				<div
+					className={"rounded-circle redLight " + turnOnRed}
+					onClick={() => setClickedLight("red")}
+				/>
+				<div
+					className={"rounded-circle yellowLight " + turnOnYellow}
+					onClick={() => setClickedLight("yellow")}
+				/>
+				<div
+					className={"rounded-circle greenLight " + turnOnGreen}
+					onClick={() => setClickedLight("green")}
+				/>
 			</div>
 			<div className="lightPole" />
 			<div className="lightBase" />
